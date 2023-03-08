ee32457530919ba158716998246582e124c10910
Crimson Ho <18565286660@163.com>
2023-03-08 20:54:42 +0800

    chore: Update random normal

--- src/pyutils/random/core.py
+++ src/pyutils/random/core.py
@@ -7,7 +7,6 @@ __all__ = [
 
 def random_normal(m=0.0, std=1.0, clip=None):
     ret = np.random.normal(loc=m, scale=std)
-    print(ret)
     if clip is not None:
         if ret - m > clip * std:
             return m + clip * std
